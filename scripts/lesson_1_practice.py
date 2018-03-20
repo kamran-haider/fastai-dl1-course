c54be6cd9f5de000bb067fce61cf486699fb75e5
kamran-haider <kamranhaider.mb@gmail.com>
2018-03-20 01:57:53 -0400

    more practice for lesson1

--- scripts/lesson_1_practice.py
+++ scripts/lesson_1_practice.py
@@ -16,7 +16,12 @@ size = 224
 arch = resnet34
 # convert dataset into a fastai native dataset type
 data = ImageClassifierData.from_paths(PATH, tfms=tfms_from_model(arch, size))
+# build the model
 model = ConvLearner.pretrained(arch, data, precompute=True)
+# train the model
+# the model is trained on the training set
 model.fit(0.01, 2)
 
-
+# Now that the model is trained, we can take a look at its performance
+# we do this by checking results on the validation set. But first we need 
+model.predict()
